98e40d83b0d49d26f90e5e10fd112d92773aa896
sangjin149 <sangjin149@gmail.com>
2024-07-15 16:06:06 +0900

    "alias 설정 완료!!!!!!!"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import { createBrowserRouter } from "react-router-dom";
 
 import "./App.css";
-import Homepage from "pages/Homepage";
+import Homepage from "@pages/Homepage";
 
 // const router = createBrowserRouter([]);
 
 function App() {
-  return <Homepage />;
+    return <Homepage />;
 }
 
 export default App;
