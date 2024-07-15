73ccf7ba8c5e383336a10123cb411bd3592a136a
sangjin149 <sangjin149@gmail.com>
2024-07-15 12:43:02 +0900

    moving workplace

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 
-import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "pages/Homepage";
 
-const router = createBrowserRouter([]);
+// const router = createBrowserRouter([]);
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return <Homepage />;
 }
 
 export default App;
