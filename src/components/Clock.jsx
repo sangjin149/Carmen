3c7d551bd26a84feba1ee8f35cc4038f0cd120cc
sangjin149 <sangjin149@gmail.com>
2024-07-17 21:47:37 +0900

    feature: Clock component

---
+++ src/components/Clock.jsx
@@ -0,0 +1,59 @@
+import { styled } from "styled-components";
+import { useState, useEffect } from "react";
+
+function updateClock() {
+    let now = new window.Date();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const day = now.getDate().toString().padStart(2, "0");
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    return { month, day, hours, minutes, seconds };
+}
+
+export default function Clock() {
+    const [time, setTime] = useState({
+        month: "1",
+        day: "01",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
+    });
+
+    function updateTime() {
+        setTime(updateClock());
+    }
+
+    useEffect(() => {
+        const interval = setInterval(updateTime, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <Container>
+            <Date>
+                {time.month}.{time.day}
+            </Date>
+            <Time>
+                {time.hours}:{time.minutes}
+            </Time>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Date = styled.div`
+    text-align: end;
+`;
+
+const Time = styled.div`
+    height: 64px;
+    line-height: 56px;
+    font-size: 80px;
+`;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,23 @@
+import { styled } from "styled-components";
+
 import Sidebar from "@components/Sidebar.jsx";
+import Clock from "src/components/Clock";
 
 export default function Homepage() {
     return (
         <div style={{ height: "500px", display: "flex", flexDirection: "row" }}>
             <Sidebar />
-            HomePage
+            <Container>
+                <Clock />
+            </Container>
         </div>
     );
 }
+
+const Container = styled.div`
+    padding-top: 72px;
+    padding-left: 16px;
+
+    display: flex;
+    flex-direction: column;
+`;
