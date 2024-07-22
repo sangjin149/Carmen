929360aad82c6a04eadb21a46c362d1313818424
sangjin149 <sangjin149@gmail.com>
2024-07-22 13:25:09 +0900

    feature: added Clock day display

--- src/components/Homepage/Clock.jsx
+++ src/components/Homepage/Clock.jsx
@@ -6,51 +6,51 @@ import { updateClock } from "@utils/time";
 export default function Clock() {
     const [time, setTime] = useState({
         month: "1",
-        day: "01",
+        date: "00",
         hours: "00",
         minutes: "00",
         seconds: "00",
+        day: "None",
     });
-
-    const [hidden, setHidden] = useState(true);
 
     function updateTime() {
         setTime(updateClock());
     }
 
+    const timeNotUpdated = time.date === "00";
+
     useEffect(() => {
         const interval = setInterval(updateTime, 1000);
-        setHidden(false);
         return () => {
             clearInterval(interval);
         };
     }, []);
 
+    if (timeNotUpdated) return <Container>Loading</Container>;
+
     return (
         <Container>
-            {setHidden ? (
-                "HIDDEN"
-            ) : (
-                <>
-                    <Date>
-                        {time.month}.{time.day}
-                    </Date>
-                    <Time>
-                        {time.hours}:{time.minutes}
-                    </Time>
-                </>
-            )}
+            <Date>
+                {time.month}.{time.date} {time.day}
+            </Date>
+            <Time>
+                {time.hours}:{time.minutes}
+            </Time>
         </Container>
     );
 }
 
 const Container = styled.div`
+    width: 26rem;
+
     display: flex;
     flex-direction: column;
+    align-items: flex-end;
 `;
 
 const Date = styled.div`
     text-align: end;
+    font-size: 1.375rem;
 `;
 
 const Time = styled.div`
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,9 +1,37 @@
 export const updateClock = () => {
     let now = new window.Date();
     const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
+    const date = now.getDate().toString().padStart(2, "0");
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
-    return { month, day, hours, minutes, seconds };
+    let day = now.getDay();
+
+    switch (day) {
+        case 0:
+            day = "일요일";
+            break;
+        case 1:
+            day = "월요일";
+            break;
+        case 2:
+            day = "화요일";
+            break;
+        case 3:
+            day = "수요일";
+            break;
+        case 4:
+            day = "목요일";
+            break;
+        case 5:
+            day = "금요일";
+            break;
+        case 6:
+            day = "토요일";
+            break;
+        default:
+            day = "날짜 계산 오류";
+    }
+
+    return { month, date, day, hours, minutes, seconds };
 };
