6c77eb28b8f4ea8def3d82fe1435f85aef939043
sangjin149 <sangjin149@gmail.com>
2024-08-11 16:02:20 +0900

    Feature: 날짜 출력 방식 조정

--- src/components/newScheduleForm/DateInput.jsx
+++ src/components/newScheduleForm/DateInput.jsx
@@ -9,7 +9,8 @@ export default function DateInput(params) {
 
     return (
         <DatePicker
-            showTime
+            showTime={{ format: "YY-MM-DD HH:mm" }}
+            format="YY-MM-DD HH:mm"
             onChange={(value, dateString) => {
                 console.log("Selected Time: ", value);
                 console.log("Formatted Selected Time: ", dateString);
