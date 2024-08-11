0949c4bd989afcca2ff25da6ac5b0292d3128e60
sangjin149 <sangjin149@gmail.com>
2024-08-11 21:35:29 +0900

    Feature: DateInput의 키보드 입력을 막는 기능 추가

--- src/components/newScheduleForm/DateInput.jsx
+++ src/components/newScheduleForm/DateInput.jsx
@@ -3,6 +3,12 @@ import { styled } from "styled-components";
 import { DatePicker } from "antd";
 
 export default function DateInput(params) {
+    //TODO: 키보드 입력 거름망
+    //TODO: 날짜 최대 최소 설정
+    //TODO: Form과 연결(param 설정)
+    //TODO: Label 클릭시 다시 열리는 현상 수정
+    //TODO: enter 입력시 제출하는 현상 수정
+
     return (
         <DatePicker
             showTime={{ format: "YY-MM-DD HH:mm" }}
@@ -14,6 +20,7 @@ export default function DateInput(params) {
             style={InputStyle}
             variant="borderless"
             size="large"
+            inputReadOnly={true}
         />
     );
 }
