7d892433e762f35c519ce37b842c54f6d13d899e
sangjin149 <sangjin149@gmail.com>
2024-08-14 21:15:05 +0900

    Feature: clickAwayContext 생성

---
+++ src/contexts/clickAwayContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const clickAwayDefault = { clickedElement: null };
+
+const clickAwayContext = createContext(clickAwayDefault);
+
+export default clickAwayContext;
