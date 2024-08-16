80260c4c7f0d67bf52bb3cf6ca02b57d17a211cf
sangjin149 <sangjin149@gmail.com>
2024-08-16 22:42:54 +0900

    Feature: DescriptionInput 구현

---
+++ src/components/newScheduleForm/DescriptionInput.jsx
@@ -0,0 +1,56 @@
+import { styled } from "styled-components";
+import { useRef, useState } from "react";
+
+export default function DescriptionInput({ ...props }) {
+    const ref = useRef();
+    const [inputHeight, setInputHeight] = useState(0);
+
+    const style = props.style ?? {};
+    const typoStyle = {
+        width: style.width,
+        fontFamily: style.fontFamily,
+        fontSize: style.fontSize,
+        lineHieght: style.lineHieght,
+    };
+
+    function handleChange(e) {
+        const realTextarea = e.target;
+        const newValue = realTextarea.value;
+        ref.current.value = newValue;
+        setInputHeight(ref.current.scrollHeight);
+    }
+
+    return (
+        <Container>
+            <RealTextarea style={{ height: inputHeight }} onChange={handleChange} {...props} />
+            <HiddenTextarea style={typoStyle} ref={ref} readOnly />
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    position: relative;
+`;
+
+const RealTextarea = styled.textarea`
+    width: 100%;
+    resize: none;
+    font-family: "NanumGothic";
+    overflow: hidden;
+    font-size: 14px;
+    line-height: 22px;
+    border: none;
+`;
+
+const HiddenTextarea = styled.textarea`
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    visibility: hidden;
+    width: 100%;
+    resize: none;
+    font-family: "NanumGothic";
+    overflow: hidden;
+    font-size: 14px;
+    line-height: 22px;
+`;
