831062dd5ed5d375b7e8b0515c91e5e054eef6e8
sangjin149 <sangjin149@gmail.com>
2024-08-18 20:53:00 +0900

    Feature: 새 SVG 컴포넌트 추가

--- src/ui/Svg2.jsx
+++ src/ui/Svg2.jsx
@@ -1,5 +1,16 @@
-export default function Svg({ src, alt, ...props }) {
+export default function Svg({ src, alt, width, height, scale = 1, top = 0, left = 0, viewBox, ...props }) {
     const Src = src;
 
-    return <Src alt={alt} {...props} />;
+    const viewBoxWidth = scale > 0 ? width / scale : width;
+    const viewBoxHeight = scale > 0 ? height / scale : height;
+
+    return (
+        <Src
+            alt={alt}
+            width={width}
+            height={height}
+            viewBox={viewBox ?? `${-left} ${-top} ${viewBoxWidth} ${viewBoxHeight}`}
+            {...props}
+        />
+    );
 }
