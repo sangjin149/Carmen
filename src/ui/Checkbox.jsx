ae3f068c5072332d8f9137df6a2e4ff3c0b52323
sangjin149 <sangjin149@gmail.com>
2024-08-19 17:23:48 +0900

    Fix: Hompage에 Icon 컴포넌트 이식

--- src/ui/Checkbox.jsx
+++ src/ui/Checkbox.jsx
@@ -1,66 +1,62 @@
 import { styled } from "styled-components";
-import Svg from "@ui/Svg";
 import { Check } from "@icons";
+import { Svg } from "@ui";
 
 export default function Chekcbox({
-  id = "group1-checkbox",
-  name = "group1 complete",
-  checked = null,
-  checkedColor = "#ff0000",
-  ...props
+    id = "group1-checkbox",
+    name = "group1 complete",
+    checked = null,
+    checkedColor = "#ff0000",
+    ...props
 }) {
-  return (
-    <Container className="checkbox" htmlFor={id}>
-      <Checkbox
-        id={id}
-        type="checkbox"
-        value={true}
-        name={name}
-        checked={checked}
-        $checkedColor={checkedColor}
-        {...props}
-      />
-      <Svg src={Check} containerStyle={CheckMarkContainerStyle} alt="checked" />
-    </Container>
-  );
+    return (
+        <Container className="checkbox" htmlFor={id}>
+            <Checkbox
+                id={id}
+                type="checkbox"
+                value={true}
+                name={name}
+                checked={checked}
+                $checkedColor={checkedColor}
+                {...props}
+            />
+            <Svg className="checkmark" src={Check} alt="checked" />
+        </Container>
+    );
 }
 
 const Container = styled.label`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 `;
 
-const CheckMarkContainerStyle = {
-  position: "absolute",
-  top: "3px",
-  left: "2px",
-};
-
 const Checkbox = styled.input`
-  -webkit-appearance: none;
-  appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
 
-  width: 1rem;
-  height: 1rem;
-  margin: 0;
-  margin-right: 0.25rem;
-  border: none;
-  border-radius: 0.25rem;
+    width: 1rem;
+    height: 1rem;
+    margin: 0;
+    margin-right: 0.25rem;
+    border: none;
+    border-radius: 0.25rem;
 
-  background-color: #d9d9d9;
-  font: inherit;
+    background-color: #d9d9d9;
+    font: inherit;
 
-  & + div {
-    display: none;
-  }
-
-  &:checked {
-    background-color: ${({ $checkedColor }) => $checkedColor};
-    & + div {
-      display: block;
-      color: white;
+    & + .checkmark {
+        visibility: hidden;
+        position: absolute;
+        fill: white;
+        left: 1px;
     }
-  }
+
+    &:checked {
+        background-color: ${({ $checkedColor }) => $checkedColor};
+        & + .checkmark {
+            visibility: visible;
+        }
+    }
 `;
--- src/ui/Icon.jsx
+++ src/ui/Icon.jsx
@@ -1,18 +1,20 @@
 import { Svg } from "@ui";
 import { styled } from "styled-components";
 
-export default function Icon({ src, iconWidth, iconHeight, size, ...props }) {
-    const width = size ?? iconWidth;
-    const height = size ?? iconHeight;
+export default function Icon({ src, containerWidth, containerHeight, size, ...props }) {
+    const width = size ?? containerWidth;
+    const height = size ?? containerHeight;
 
     return (
-        <Container {...props}>
-            <Svg src={src} width={width} height={height} />
+        <Container width={width} height={height}>
+            <Svg src={src} {...props} />
         </Container>
     );
 }
 
 const Container = styled.div`
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
     display: flex;
     justify-content: center;
     align-items: center;
