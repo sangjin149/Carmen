f81a74c2c6be4af4d60de477520260c57e51f568
sangjin149 <sangjin149@gmail.com>
2024-08-20 16:18:45 +0900

    Misc: GroupInputItem 컴포넌트 분리

--- src/components/newScheduleForm/GroupInputItem.jsx
+++ src/components/newScheduleForm/GroupInputItem.jsx
@@ -2,15 +2,23 @@ import { Dot } from "@icons";
 import { Icon } from "@ui";
 import { styled } from "styled-components";
 
-export default function GroupInputItem({ description, groupColor = "#000000" }) {
+export default function GroupInputItem({ name, groupColor = "#000000" }) {
     return (
-        <Container>
-            <Icon src={Dot} style={{ fill: groupColor }} />
-            {description}
-        </Container>
+        <ItemContainer>
+            <GroupColorIcon src={Dot} containerWidth={14} size={14} fill={groupColor} />
+            {name}
+        </ItemContainer>
     );
 }
 
-const Container = styled.div`
+const ItemContainer = styled.div`
     display: flex;
+    width: 4rem;
+    height: 1.5rem;
+    align-items: center;
+`;
+
+const GroupColorIcon = styled(Icon)`
+    margin-right: 4px;
+    fill: ${(props) => props.fill};
 `;
