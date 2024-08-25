eaf7480706ec8e80615d2a52b4c9939c38df50f1
sangjin149 <sangjin149@gmail.com>
2024-08-25 16:10:13 +0900

    Config: ESlint 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import stylisticjs from '@stylistic/eslint-plugin-js';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { name: 'Carmen Lint'},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      '@stylistic/js': stylisticjs,
+    },
+  },
 ];
--- src/components/Homepage/Clock.jsx
+++ src/components/Homepage/Clock.jsx
@@ -1,63 +1,67 @@
-import { styled } from "styled-components";
-import { useState, useEffect } from "react";
+import { styled } from 'styled-components';
+import { useState, useEffect } from 'react';
 
-import { updateClock } from "@utils/time";
+import { updateClock } from '@utils/time';
 
 export default function Clock() {
-    const [time, setTime] = useState({
-        month: "1",
-        date: "00",
-        hours: "00",
-        minutes: "00",
-        seconds: "00",
-        day: "None",
-    });
+  const [time, setTime] = useState({
+    month: '1',
+    date: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+    day: 'None',
+  });
+ 
+  function updateTime() {
+    setTime(updateClock());
+  }
 
-    function updateTime() {
-        setTime(updateClock());
-    }
+  const timeNotUpdated = time.date === '00';
 
-    const timeNotUpdated = time.date === "00";
+  useEffect(() => {
+    const interval = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    useEffect(() => {
-        const interval = setInterval(updateTime, 1000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
-    return (
-        <Container>
-            <Date>{timeNotUpdated ? "" : `${time.month}.${time.date} ${time.day}`}</Date>
-            <Time>
-                {timeNotUpdated ? <LoadingIndicator>Loading...</LoadingIndicator> : `${time.hours}:${time.minutes}`}
-            </Time>
-        </Container>
-    );
+  return (
+    <Container>
+      <Date>{timeNotUpdated ? '' : `${time.month}.${time.date} ${time.day}`}</Date>
+      <Time>
+        {timeNotUpdated ? (
+          <LoadingIndicator>Loading...</LoadingIndicator>
+        ) : (
+          `${time.hours}:${time.minutes}`
+        )}
+      </Time>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    height: 6rem;
-    width: 26rem;
+  height: 6rem;
+  width: 26rem;
 
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: flex-end;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
 `;
 
 const Date = styled.div`
-    height: 2rem;
-    text-align: end;
-    font-size: 1.25rem;
+  height: 2rem;
+  text-align: end;
+  font-size: 1.25rem;
 `;
 
 const Time = styled.div`
-    height: 4rem;
-    line-height: 4.5rem;
-    font-size: 5rem;
+  height: 4rem;
+  line-height: 4.5rem;
+  font-size: 5rem;
 `;
 
 const LoadingIndicator = styled.div`
-    font-size: 5rem;
+  font-size: 5rem;
 `;
