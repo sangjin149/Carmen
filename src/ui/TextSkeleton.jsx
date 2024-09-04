45b4da093b50ac9fd3d5017211b46d1a7c6a636f
sangjin149 <sangjin149@gmail.com>
2024-09-04 17:58:37 +0900

    Feature: TextSkeleton 구현

--- src/ui/TextSkeleton.jsx
+++ src/ui/TextSkeleton.jsx
@@ -1,1 +1,26 @@
-export default function TextSkeleton(params) {}
+import { styled } from 'styled-components';
+import { Skeleton } from '@ui';
+
+export default function TextSkeleton({ lineSize = 16, lines = 1, lineProps, ...props }) {
+  const arr = [];
+  for (let i = 0; i < lines; i++) arr.push(`skeleton line ${i}`);
+
+  return (
+    <Container {...props}>
+      {arr.map((val) => (
+        <LineSkeleton key={val} $linesize={lineSize} {...lineProps} />
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const LineSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: ${(props) => props.$linesize}px;
+`;
