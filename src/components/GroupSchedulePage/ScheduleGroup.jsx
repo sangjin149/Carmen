ab1d27fabace1739f9d614d92506cb4f1cbe192d
sangjin149 <sangjin149@gmail.com>
2024-11-06 21:56:32 +0900

    스케쥴 그룹 하위 컴포넌트 생성

--- src/components/GroupSchedulePage/ScheduleGroup.jsx
+++ src/components/GroupSchedulePage/ScheduleGroup.jsx
@@ -1,5 +1,14 @@
 import { styled } from 'styled-components';
 
-export default function ScheduleGroup({ children }) {
-  return <div></div>;
+export default function ScheduleGroup({ title, children }) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      {children}
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const Title = styled.div``;
