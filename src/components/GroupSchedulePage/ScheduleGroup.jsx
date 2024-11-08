55d0283bf56525ee74975ddc692ece0ed56988e8
sangjin149 <sangjin149@gmail.com>
2024-11-08 23:36:33 +0900

    누락 컴포넌트 추가

--- src/components/GroupSchedulePage/ScheduleGroup.jsx
+++ src/components/GroupSchedulePage/ScheduleGroup.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@ui';
 import { styled } from 'styled-components';
 
 export default function ScheduleGroup({ title, children }) {
@@ -10,5 +11,7 @@ export default function ScheduleGroup({ title, children }) {
 }
 
 const Container = styled.div``;
+
+const FoldButton = styled(Button)``;
 
 const Title = styled.div``;
